8365dc638df2d558584503edb23ae58cdac2601f
Sean Morris <seanj.morris@gmail.com>
2025-08-17 07:59:11 -0400

    Cleanup dashboard.py.

--- page_objects/dashboard.py
+++ page_objects/dashboard.py
@@ -1,5 +1,3 @@
-import sys
-import time
 from playwright.sync_api import sync_playwright, Page, TimeoutError, expect  # type: ignore
 from date_variables import *
 
@@ -32,21 +30,6 @@ class Dashboard():
     def deal_with_modal_popups(self):
         self.exit_modal_of_ad_if_applicable()
         self.exit_modal_of_code_of_conduct_if_applicable()
-
-    def navigate_to_sessions_starting_on(self, datetime_input):
-        url_base = "https://playtimescheduler.com/index.php?go=next&startDate="
-        url_for_next_weeks_session = url_base + get_one_week_earlier_url_string(datetime_input)
-        self.page.goto(url_for_next_weeks_session)
-
-    def navigate_to_next_weeks_sessions(self):
-        """
-        It's possible to access a specific date range by clicking on the calendar button and then selecting the date, but this is more problematic programmatically. Instead, we can simply enter in the URL for the range in question, which appears in the form:
-        `https://playtimescheduler.com/index.php?go=next&startDate=2025-06-04`
-        Where the date at the end of the string is the date 1 week before the date we want to access. So, the URL shown above would show the week starting on June 11, 2025 and ending a week later on June 18, 2025.
-        """
-        url_base = "https://playtimescheduler.com/index.php?go=next&startDate="
-        url_for_next_weeks_session = url_base + get_date_one_week_from_today()
-        self.page.goto(url_for_next_weeks_session)
 
     def select_next_weeks_session(self):
         # Check if the link is visible
