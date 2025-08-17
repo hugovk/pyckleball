31edad8901dd1677c3233c45cfa7b4d7db60399d
Sean Morris <seanj.morris@gmail.com>
2025-08-17 08:26:06 -0400

    Remove code to dismiss the 'code of conduct' modal since this modal has never appeared when logging in via playwright.

--- page_objects/dashboard.py
+++ page_objects/dashboard.py
@@ -16,16 +16,5 @@ class Dashboard():
         except Exception as e:
             print(f"An unexpected error occurred with the ad modal: {e}")
 
-    # def exit_modal_of_code_of_conduct_if_applicable(self):
-    #     try:
-    #         got_it_button = self.page.get_by_role("button", name="Got It!", exact=True)
-    #         got_it_button.click(timeout=2000)  # Set timeout to 3000ms
-    #         print("Successfully closed the 'Code of Conduct' modal...")
-    #     except TimeoutError:
-    #         print("The 'Code of Conduct' modal did not appear or was not interactable within 3 seconds, moving on...")
-    #     except Exception as e:
-    #         print(f"An unexpected error occurred with the 'Code of Conduct' modal: {e}")
-
     def deal_with_modal_popups(self):
         self.exit_modal_of_ad_if_applicable()
-        self.exit_modal_of_code_of_conduct_if_applicable()
