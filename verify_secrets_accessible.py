edc868c756464e30f20680f0e2ca493f97cdb7dc
Sean Morris <seanj.morris@gmail.com>
2025-08-19 18:20:46 -0400

    Replace uv pip install with uv sync.

--- verify_secrets_accessible.py
+++ verify_secrets_accessible.py
@@ -1,2 +1,4 @@
+
 if __name__ == "__main__":
-    print(f"SEAN LOADED SECRETS SUCCESSFULLY - and my biggest secret is... '{BIGGEST_SECRET}'")
+    print("Trying to verify secrets are loaded...")
+    # print(f"SEAN LOADED SECRETS SUCCESSFULLY - and my biggest secret is... '{BIGGEST_SECRET}'")
