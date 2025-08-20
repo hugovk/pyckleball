b6af8b3eef0d23495b3bdcb858c1b020fc4d2f21
Sean Morris <seanj.morris@gmail.com>
2025-08-20 19:13:49 -0400

    Use os.getenv instead of os.environ.

--- verify_secrets_accessible2.py
+++ verify_secrets_accessible2.py
@@ -1,7 +1,7 @@
 import os
 
-MY_SUPER_REPO_SECRET = os.environ("my_super_repo_secret")
-MY_BIGGEST_SECRET = os.environ("my_biggest_secret")
+MY_SUPER_REPO_SECRET = os.getenv("my_super_repo_secret")
+MY_BIGGEST_SECRET = os.getenv("my_biggest_secret")
 
 print(f"MY_SUPER_REPO_SECRET is: {MY_SUPER_REPO_SECRET}")
 print(f"MY_BIGGEST_SECRET is: {MY_BIGGEST_SECRET}")
