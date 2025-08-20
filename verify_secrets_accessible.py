21ec1e530da489ed90b18256c28bcc0fc5ce5c16
Sean Morris <seanj.morris@gmail.com>
2025-08-19 22:38:49 -0400

    Test secret accessibility.

--- verify_secrets_accessible.py
+++ verify_secrets_accessible.py
@@ -1,4 +1,10 @@
+import os
+from dotenv import load_dotenv, find_dotenv
 
-if __name__ == "__main__":
-    print("Trying to verify secrets are loaded...")
-    # print(f"SEAN LOADED SECRETS SUCCESSFULLY - and my biggest secret is... '{BIGGEST_SECRET}'")
+load_dotenv(find_dotenv(), override=True)
+
+print("Trying to verify secrets are loaded...")
+biggest_secret1 = os.getenv("BIGGEST_SECRET")
+print(f"SEAN LOADED SECRETS SUCCESSFULLY - and my biggest secret is... '{biggest_secret1}'")
+super_repo_secret1 = os.getenv("SUPER_REPO_SECRET")
+print(f"SUPER REPO SECRET is: {super_repo_secret1}")
