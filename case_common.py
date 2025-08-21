5060a4927f0357f603171a56bb448dcec79ec9c8
Sean Morris <seanj.morris@gmail.com>
2025-08-20 20:33:38 -0400

    3rd attempt with just load_dotenv()

--- case_common.py
+++ case_common.py
@@ -21,8 +21,8 @@ def initialize_case(user_type: Literal["pro", "registrant"]) -> Page:
     """
 
     # load_dotenv(find_dotenv(), override=True)
-    load_dotenv(find_dotenv(), override=False)
-    # load_dotenv()
+    # load_dotenv(find_dotenv(), override=False)
+    load_dotenv()
     if user_type == "pro":
         username = os.getenv("PRO_USER_NAME")
         password = os.getenv("PRO_USER_PASSWORD")
@@ -34,8 +34,8 @@ def initialize_case(user_type: Literal["pro", "registrant"]) -> Page:
         raise ValueError(f"Environment variables {username} and/or {password} are not set.")
 
     playwright = sync_playwright().start()
-    # browser = playwright.chromium.launch(headless=False, slow_mo=500)
-    browser = playwright.chromium.launch(headless=True)
+    browser = playwright.chromium.launch(headless=False, slow_mo=500)
+    # browser = playwright.chromium.launch(headless=True)
     page = browser.new_page()
 
     # Login workflow
