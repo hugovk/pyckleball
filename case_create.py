567be16c8c009d92898b038ea1e9853738355e20
Sean Morris <seanj.morris@gmail.com>
2025-08-23 09:22:04 -0400

    Refactor case_create.py and case_delete.py with context manager timer.

--- case_create.py
+++ case_create.py
@@ -5,32 +5,35 @@ from case_common import initialize_case
 from print_with_color import print_success, print_blue, print_yellow
 from date_variables import case_day, case_day_my_session_string, case_day_start_time, case_day_end_time
 from date_picker_handler import date_picker_handler
+from timer import Timer
 
 def case_create():
     print(f"ATTEMPT: Case_create.py is creating pro session for {case_day_my_session_string}.")
-    dashboard = initialize_case("pro")
-    page = dashboard
-    page.get_by_role("button", name="+ Add Session").click()
 
-    page.locator("#session_location").select_option("1082") # Woburn Racquet Club
-    date_picker_handler(page, page.click("#session_date"), case_day)
-    page.locator("#session_time").click()
-    page.locator("#session_time").select_option(case_day_start_time)
-    page.locator("#session_time_end").click()
-    page.locator("#session_time_end").select_option(case_day_end_time)
-    page.locator("#session_waitlist").select_option("0")
-    page.locator("#session_title").click()
-    page.locator("#session_title").fill("SINGLES MATCH")
-    # page.pause()
-    page.locator("#session_release").select_option("0") # option to open the session for sign ups immediately
-    # page.locator("#session_release").select_option("1") # option to open the session for sign ups 24 hours in advance
-    page.get_by_role("radio", name="No").check()
-    page.get_by_role("textbox").nth(2).click()
-    page.get_by_role("textbox").nth(2).fill("Singles session.")
-    page.get_by_role("checkbox", name="I acknowledge that a play").check()
-    page.pause()
-    page.get_by_role("button", name="Add Session").click()
-    # print(f"SUCCESS: Case_create.py created session for {case_day_my_session_string}.")
+    with Timer() as timer:
+        dashboard = initialize_case("pro")
+        page = dashboard
+        page.get_by_role("button", name="+ Add Session").click()
+
+        page.locator("#session_location").select_option("1082") # Woburn Racquet Club
+        date_picker_handler(page, page.click("#session_date"), case_day)
+        page.locator("#session_time").click()
+        page.locator("#session_time").select_option(case_day_start_time)
+        page.locator("#session_time_end").click()
+        page.locator("#session_time_end").select_option(case_day_end_time)
+        page.locator("#session_waitlist").select_option("0")
+        page.locator("#session_title").click()
+        page.locator("#session_title").fill("SINGLES MATCH")
+        # page.pause()
+        page.locator("#session_release").select_option("0") # option to open the session for sign ups immediately
+        # page.locator("#session_release").select_option("1") # option to open the session for sign ups 24 hours in advance
+        page.get_by_role("radio", name="No").check()
+        page.get_by_role("textbox").nth(2).click()
+        page.get_by_role("textbox").nth(2).fill("Singles session.")
+        page.get_by_role("checkbox", name="I acknowledge that a play").check()
+        page.pause()
+        page.get_by_role("button", name="Add Session").click()
+        # print(f"SUCCESS: Case_create.py created session for {case_day_my_session_string}.")
     print_success(f"SUCCESS: Case_create.py created session for {case_day_my_session_string}.")
 
 if __name__ == "__main__":
--- case_delete.py
+++ case_delete.py
@@ -4,17 +4,18 @@
 from case_common import initialize_case
 from print_with_color import print_success, print_blue, print_yellow
 from date_variables import case_day_my_session_string
-
+from timer import Timer
 
 def case_delete():
     print(f"ATTEMPT: Case_delete.py is deleting pro session for {case_day_my_session_string}.")
-    page = initialize_case("pro")
-    page.get_by_role("button", name="My Sessions").click()
-    page.get_by_role("link", name=case_day_my_session_string).click()
-    page.get_by_role("link", name="Edit/Cancel Session...").click()
-    page.get_by_role("button", name="CANCEL SESSION").click()
-    page.get_by_role("button", name="Cancel Session", exact=True).click()
-    # print(f"SUCCESS: Case_delete.py deleted session for {case_day_my_session_string}.")
+    with Timer() as timer:
+        page = initialize_case("pro")
+        page.get_by_role("button", name="My Sessions").click()
+        page.get_by_role("link", name=case_day_my_session_string).click()
+        page.get_by_role("link", name="Edit/Cancel Session...").click()
+        page.get_by_role("button", name="CANCEL SESSION").click()
+        page.get_by_role("button", name="Cancel Session", exact=True).click()
+
     print_success(f"SUCCESS: Case_delete.py deleted session for {case_day_my_session_string}.")
 
 if __name__ == "__main__":
