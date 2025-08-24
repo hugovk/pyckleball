eb0c723f0c0edc915064c45328b291e9a7fbc2a4
Sean Morris <seanj.morris@gmail.com>
2025-08-24 10:54:53 -0400

    Refactor date_variables.py to improve session string formatting and enhance output clarity; add print statements for better debugging.

--- date_variables.py
+++ date_variables.py
@@ -1,27 +1,21 @@
 from datetime import datetime, timedelta
+from print_with_color import print_blue
 
 def produce_case_day_strings(case_day: datetime):
     case_day_end_datetime = case_day + timedelta(minutes=30)
 
+    # String on "My Sessions" dashboard
+    my_session_string = f"{case_day.strftime('%a, %b %-d @ %-I:%M')}-{case_day_end_datetime.strftime('%-I:%M%p')}"  # Output: Ddd, Mmm d @ h:mm-h:mmAM
+
     # Formatting the start and end times for "+ Add Session" workflow
     add_session_start_time = case_day.strftime("%I:%M %p")  # Output: hh:mm PM
     add_session_end_time = case_day_end_datetime.strftime("%I:%M %p")  # Output: hh:mm PM
-
-    # String on "My Sessions" dashboard
-    my_session_string = f"{case_day.strftime('%a, %b %-d @ %-I:%M')}-{case_day_end_datetime.strftime('%-I:%M%p')}"  # Output: Ddd, Mmm d @ h:mm-h:mmAM
 
     # Date string for registering in bubble or list mode - e.g. '1:45P'
     hmmAP  = case_day.strftime("%-I:%M%p")[:-1]
     for_registering_bubble_mode = "4.0-5.0 " + hmmAP
     for_registering_list_mode = hmmAP + " : Bedford - John Glenn Middle School – 4.0-5.0"
 
-    # return {
-    #     'add_session_start_time': add_session_start_time,
-    #     'add_session_end_time': add_session_end_time,
-    #     'my_session_string': my_session_string,
-    #     'for_registering_bubble_mode': for_registering_bubble_mode,
-    #     'for_registering_list_mode': for_registering_list_mode
-    # }
     return {
         'case_day': case_day,
         'add_session_start_time': add_session_start_time,
@@ -62,30 +56,45 @@ def get_url_for_session_on(datetime_input: datetime):
     return full_url
 
 if __name__ == "__main__":
+    print_blue("FOR RIGHT NOW")
+    case_day = datetime.now()
+    case_day_strings = produce_case_day_strings(case_day)
+    print(f"case_day_strings['my_session_string']--------------------{case_day_strings['my_session_string']}")
+    print(f"case_day_strings['add_session_start_time']---------------{case_day_strings['add_session_start_time']}")
+    print(f"case_day_strings['add_session_end_time']-----------------{case_day_strings['add_session_end_time']}")
+    print(f"case_day_strings['for_registering_bubble_mode']----------{case_day_strings['for_registering_bubble_mode']}")
+    print(f"case_day_strings['for_registering_list_mode']------------{case_day_strings['for_registering_list_mode']}")
+    print(f"get_url_for_session_on(case_day)-------------------------{get_url_for_session_on(case_day)}")
+    print("")
+
+    print_blue("FOR A CASE DAY IN THE PAST:")
     case_day = datetime(1015, 10, 4, 13, 45)
     case_day_strings = produce_case_day_strings(case_day)
+    print(f"case_day_strings['my_session_string']--------------------{case_day_strings['my_session_string']}")
     print(f"case_day_strings['add_session_start_time']---------------{case_day_strings['add_session_start_time']}")
     print(f"case_day_strings['add_session_end_time']-----------------{case_day_strings['add_session_end_time']}")
-    print(f"case_day_strings['my_session_string']--------------------{case_day_strings['my_session_string']}")
     print(f"case_day_strings['for_registering_bubble_mode']----------{case_day_strings['for_registering_bubble_mode']}")
     print(f"case_day_strings['for_registering_list_mode']------------{case_day_strings['for_registering_list_mode']}")
     print(f"get_url_for_session_on(case_day)-------------------------{get_url_for_session_on(case_day)}")
     print("")
-    print("CASE DAY NEXT 24-HR SIGN UP OPPORTUNITY...")
+
+    print_blue("NEXT 24-HR SIGN UP SESSION:")
     case_day = day_next_sign_up_opp_24_hours()
     case_day_strings = produce_case_day_strings(case_day)
+    print(f"case_day_strings['my_session_string']--------------------{case_day_strings['my_session_string']}")
     print(f"case_day_strings['add_session_start_time']---------------{case_day_strings['add_session_start_time']}")
     print(f"case_day_strings['add_session_end_time']-----------------{case_day_strings['add_session_end_time']}")
-    print(f"case_day_strings['my_session_string']--------------------{case_day_strings['my_session_string']}")
     print(f"case_day_strings['for_registering_bubble_mode']----------{case_day_strings['for_registering_bubble_mode']}")
     print(f"case_day_strings['for_registering_list_mode']------------{case_day_strings['for_registering_list_mode']}")
     print(f"get_url_for_session_on(case_day)-------------------------{get_url_for_session_on(case_day)}")
     print("")
+
+    print_blue("MOMENT TO SIGN UP FOR TOMORROW'S 24-HR SIGN UP SESSION:")
     case_day = sign_up_today_for_session_in_24_hours()
     case_day_strings = produce_case_day_strings(case_day)
+    print(f"case_day_strings['my_session_string']--------------------{case_day_strings['my_session_string']}")
     print(f"case_day_strings['add_session_start_time']---------------{case_day_strings['add_session_start_time']}")
     print(f"case_day_strings['add_session_end_time']-----------------{case_day_strings['add_session_end_time']}")
-    print(f"case_day_strings['my_session_string']--------------------{case_day_strings['my_session_string']}")
     print(f"case_day_strings['for_registering_bubble_mode']----------{case_day_strings['for_registering_bubble_mode']}")
     print(f"case_day_strings['for_registering_list_mode']------------{case_day_strings['for_registering_list_mode']}")
     print(f"get_url_for_session_on(case_day)-------------------------{get_url_for_session_on(case_day)}")
