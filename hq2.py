b00fe82003f807eb33161f2fa957c30a7db18a8d
Sean Morris <seanj.morris@gmail.com>
2025-08-24 21:18:33 -0400

    Big attempt #2 let's go (trying to sign up for session on 8/24 9:30 PM for a session on 8/25).

--- hq2.py
+++ hq2.py
@@ -4,7 +4,7 @@ from date_variables import day_next_sign_up_opp_24_hours
 
 if __name__ == "__main__":
 
-    case_day = datetime(2025, 8, 25, 21, 00)
+    case_day = datetime(2025, 8, 25, 21, 30)
     # case_day = day_next_sign_up_opp_24_hours()
     headless = True
     sign_up_24_hr_advance = True
